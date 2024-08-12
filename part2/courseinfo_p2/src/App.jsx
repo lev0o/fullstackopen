eb371f9e1d1cce46bca8f706aac49da1f3bb98fe
lev0o <abdullahmohdm02@gmail.com>
2024-08-12 22:45:44 +0400

    Completed exercises 2.2 and 2.3

--- part2/courseinfo_p2/src/App.jsx
+++ part2/courseinfo_p2/src/App.jsx
@@ -9,13 +9,14 @@ const Part = ({ part }) =>
 
 const Content = ({ parts }) => 
   <>
-    {parts.map(part => <Part part={part} />)}
+    {parts.map(part => <Part key={part.id} part={part} />)}
   </>
 
 const Course = ({ course }) => 
   <>
     <Header course={course.name} />
     <Content parts={course.parts} />
+    <Total sum={course.parts.reduce((acc, part) => acc += part.exercises, 0)}/>
   </>
 
 const App = () => {
